1df53582e0c1b9eda3f74293816473662e79486c
skywong14 <skywong0014@gmail.com>
2025-06-02 00:52:01 +0800

    update user programs

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -6,9 +6,18 @@ pub mod console;
 mod lang_items;
 mod syscall;
 
+use buddy_system_allocator::LockedHeap;
+#[global_allocator]
+static HEAP: LockedHeap = LockedHeap::empty();
+
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".text.entry")]
 pub extern "C" fn _start() -> ! {
+    // init HEAP
+    unsafe {
+        HEAP.lock()
+            .init(addr_of_mut!(HEAP_SPACE) as usize, USER_HEAP_SIZE);
+    }
     exit(main());
     panic!("unreachable after sys_exit!");
 }
@@ -20,6 +29,10 @@ fn main() -> i32 {
 }
 
 use syscall::*;
+
+pub fn read(fd: usize, buf: &mut [u8]) -> isize {
+    sys_read(fd, buf)
+}
 
 pub fn write(fd: usize, buf: &[u8]) -> isize {
     sys_write(fd, buf)
@@ -33,3 +46,34 @@ pub fn yield_() -> isize { sys_yield() }
 pub fn get_time() -> usize { sys_get_time() }
 
 pub fn sbrk(size: i32) -> isize { sys_sbrk(size) }
+
+pub fn getpid() -> isize { sys_getpid() }
+
+pub fn fork() -> isize { sys_fork() }
+
+pub fn exec(path: &str) -> isize { sys_exec(path) }
+
+pub fn wait(exit_code: &mut i32) -> isize {
+    loop {
+        match sys_waitpid(-1, exit_code as *mut _) {
+            -2 => { yield_(); } // Exist child processes, but no child processes have exited yet
+            exit_pid => return exit_pid, // -1 (no child processes) or a real pid
+        }
+    }
+}
+
+pub fn waitpid(pid: usize, exit_code: &mut i32) -> isize {
+    loop {
+        match sys_waitpid(pid as isize, exit_code as *mut _) {
+            -2 => { yield_(); }  // Exist child processes, but no child processes have exited yet
+            exit_pid => return exit_pid, // -1 (no child processes) or a real pid
+        }
+    }
+}
+
+pub fn sleep(period_ms: usize) {
+    let start = sys_get_time();
+    while sys_get_time() < start + period_ms {
+        sys_yield();
+    }
+}
